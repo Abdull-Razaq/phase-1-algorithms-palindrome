2600fae6baec6dfeb82b3cea44e830e2264fc032
Abdirizak Mohamed <abdirizakm123@gmail.com>
2022-12-13 23:19:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord = word.split("").reverse().join("");
+
+  if (word === reverseWord) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  Code to check whether words are of palindrome nature
+  Check whether a string passed is a palindrome; if a palindrome passes true else passes false
+  let isPalindrome = string;
+  if (string == Palindrome){ return true }
+  else ( rerturn false)
 */
 
 /*
   Add written explanation of your solution here
+  The above code receives a string and checks whether the string is of Palindrome nature; if it fulfils the condition a true boolean value is passed and the vice versa is true.
 */
 
 // You can run `node index.js` to view these console logs
